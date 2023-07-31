5cadcf67279ccf67b0a914b0a78e61a8f92beefa
prateek <prateekrath2023@gmail.com>
2023-07-31 10:14:24 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,13 @@
+const amount = 12;
+if (amount < 10) {
+  console.log("small");
+} else {
+  console.log("large");
+}
+
+console.log(`hey it is my first node app!!!`);
+console.log(__dirname);
+console.log(__filename);
+console.log(require);
+console.log(module);
+console.log(process);
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+const secret = "SUPER SECRET"; // local
+
+const john = "john"; //these we will share
+const peter = "peter";
+
+console.log(module);
+
+module.exports = { john, peter };
